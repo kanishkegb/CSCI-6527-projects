5ea214959ebc623bfb7dc84a5036e3520e5aac2f
Kanishke-XPS <kanishkegb@gwu.edu>
2018-01-25 11:20:01 -0500

    BGR separating and combining works

--- Project-1/process_image.py
+++ Project-1/process_image.py
@@ -1,17 +1,36 @@
+from matplotlib import pyplot as plt
+
 import cv2
 import numpy as np
 import pdb
 
 
-img = cv2.imread('00608r.jpg', 0)
+img_full = cv2.imread('00608r.jpg', 0)
+
+# crop the original image to remove outer white border
+t, b = 10, 7
+l, r = 2, 2
+img = img_full[t:img_full.shape[0] - b, l:img_full.shape[1] - r]
 
 img_h, img_w = img.shape
+split_h = int(img_h / 3)
 
-img_b = img.copy()[:int(img_h / 3), :]
-img_g = img.copy()[int(img_h / 3):int(img_h * 2 / 3), :]
-img_r = img.copy()[int(img_h * 2 / 3):, :]
+img_b = img.copy()[:split_h, :]
+img_g = img.copy()[split_h:split_h * 2, :]
+img_r = img.copy()[split_h * 2:split_h * 3, :]
 
-pdb.set_trace()
+# pdb.set_trace()
 
-cv2.imshow('Image', img)
-key = cv2.waitKey(0) & 0xFF
+img_bgr = np.zeros((split_h, img_w, 3), 'uint8')
+
+# for i in range(3):
+img_bgr[:, :, 0] = img_b
+img_bgr[:, :, 1] = img_g
+img_bgr[:, :, 2] = img_r
+
+plt.subplot(141), plt.imshow(img_b,'gray'), plt.title('Blue')
+plt.subplot(142), plt.imshow(img_g,'gray'), plt.title('Green')
+plt.subplot(143), plt.imshow(img_r,'gray'), plt.title('Red')
+plt.subplot(144), plt.imshow(img_bgr,'gray'), plt.title('BGR')
+
+plt.show()
---
+++ Project-1/test.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+
+import cv2
+import numpy as np
+import pdb
+
+
+img = cv2.imread('lakeandballoon.jpg', 1)
+
+img_h, img_w, img_ch = img.shape
+
+img_b = img.copy()[:, :, 0]
+img_g = img.copy()[:, :, 1]
+img_r = img.copy()[:, :, 2]
+
+img_bgr = np.zeros((img_h, img_w, 3), 'uint8')
+
+img_bgr[:, :, 0] = img_b
+img_bgr[:, :, 1] = img_g
+img_bgr[:, :, 2] = img_r
+
+cv2.imshow("Image", img_bgr)
+key = cv2.waitKey(0) & 0xFF
