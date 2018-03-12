d7713554f29e55d21dac44c5209d4ee01b2939db
Kanishke-Desktop <kanishkegb@gmail.com>
2018-03-11 23:32:17 -0400

    generates stereogram

--- Project-3/autostereogram_generator.m
+++ Project-3/autostereogram_generator.m
@@ -1,5 +1,44 @@
-close all; % closes all figures
+close all;
+clear;
 
 addpath ./images
 
-im_depth = im2single(imread('depth.jpg'));
+depth_image = 'depth.jpg';
+im = im2double((imread(depth_image)));
+[h, w] = size(im);
+
+im_out = -1*ones(h, w);
+for r = 1:h
+    % start at the left pixel of each row
+    for c = 1:w
+        if im_out(r, c) >= 0
+            continue
+        end
+        
+        
+        x = c;
+        
+        % randomly pick color C
+        color = [rand(1), rand(1), rand(1)];
+        color = rand(1);
+        im_out(r, x) = color;
+        
+        gap = 100;
+        
+        %  map depth onto a displacement d
+        d = gap + floor(255*im(r, x)/2);
+        x = x + d;
+        
+        % if x is still in the image 
+        while x <= w 
+            % then loop to "color location x..."
+            im_out(r, x) = color;
+            
+            d = gap + floor(255*im(r, x)/2);
+            x = x + d;
+        end
+    end
+end
+% imagesc(im_out)
+% axis equal
+imshow(im_out)
