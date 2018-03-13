643129b33e50f233ae127506efb205bf4ceb852e
Kanishke-Desktop <kanishkegb@gmail.com>
2018-03-12 23:33:53 -0400

    added dance and shark

--- Project-3/autostereogram_generator.m
+++ Project-3/autostereogram_generator.m
@@ -4,30 +4,32 @@ clear;
 addpath ./images
 
 depth_image = 'depth.jpg';
-% im = im2double(rgb2gray(imread(depth_image)));
-im = im2double((imread(depth_image)));
+im_check = imread(depth_image);
+
+if length(size(im_check)) == 3
+    im = im2double(rgb2gray(imread(depth_image)));
+else
+    im = im2double(imread(depth_image));
+end
+
 % im = imcomplement(im);
 [h, w] = size(im);
 
 im_out = zeros(h, w, 3);
 for r = 1:h
-    % start at the left pixel of each row
     for c = 1:w
+        
+        % if non zero, a color is already picked
         if im_out(r, c, 1) > 0
             continue
         end
-        
-        
+
         x = c;
-        
-        % randomly pick color C
         color = [rand(1), rand(1), rand(1)];
-        % color = rand(1);
         
-        gap = floor(w/10);
+        gap = floor(w/20);
         scale = .5;
 
-        % if x is still in the image 
         while x <= w 
             im_out(r, x, :) = color;
             
