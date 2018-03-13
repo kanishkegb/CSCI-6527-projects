e4af00c3bbf51c22ccadf4486808a5079036f249
Kanishke-Desktop <kanishkegb@gmail.com>
2018-03-12 23:13:34 -0400

    comments

--- Project-3/autostereogram_generator.m
+++ Project-3/autostereogram_generator.m
@@ -4,14 +4,16 @@ clear;
 addpath ./images
 
 depth_image = 'depth.jpg';
+% im = im2double(rgb2gray(imread(depth_image)));
 im = im2double((imread(depth_image)));
+% im = imcomplement(im);
 [h, w] = size(im);
 
-im_out = -1*ones(h, w, 3);
+im_out = zeros(h, w, 3);
 for r = 1:h
     % start at the left pixel of each row
     for c = 1:w
-        if im_out(r, c, 1) >= 0
+        if im_out(r, c, 1) > 0
             continue
         end
         
@@ -21,24 +23,19 @@ for r = 1:h
         % randomly pick color C
         color = [rand(1), rand(1), rand(1)];
         % color = rand(1);
-        im_out(r, x, :) = color;
         
-        gap = 50;
-        
-        %  map depth onto a displacement d
-        d = gap + floor(255*im(r, x)/2);
-        x = x + d;
-        
+        gap = floor(w/10);
+        scale = .5;
+
         % if x is still in the image 
         while x <= w 
-            % then loop to "color location x..."
             im_out(r, x, :) = color;
             
-            d = gap + floor(255*im(r, x)/2);
+            d = gap + floor(255*im(r, x)*scale);
             x = x + d;
         end
     end
 end
 % imagesc(im_out)
 % axis equal
-imshow(im_out)
+imshow((im_out))
