acaa38e900696361569d0f7b6e2b73b9dd8bf436
Kanishke-Desktop <kanishkegb@gwu.edu>
2018-03-25 17:22:39 -0400

    added gif

--- Project-3/moving_autostereogram.m
+++ Project-3/moving_autostereogram.m
@@ -15,14 +15,29 @@ t_data = struct('h', h, 'w', w, 'x0', x0, 'y0', y0, 'max_r', max_r, ...
                 'min_r', min_r, 'num_circles', num_circles);
 
 im_tunnel = imgaussfilt(tunnel(t_data), 2);
+reso = 10;
+frames = 1:round((max_r - min_r) / reso);
 
-% for r = min_r:max_r
-    im_frame = draw_cir_on_image(im_tunnel, 50, 0, x0, y0);
+flag_first_run = 1;
+filename = 'animation.gif';
+for r = max_r:-50:min_r
+    im_frame = draw_cir_on_image(im_tunnel, round(r/5), 0, x0, y0);
     im_out = autostereogram(im_frame);
-% end
+    imshow(im_out)
+    axis equal
+    
+    [A,map] = rgb2ind(im_out, 256);
+    if flag_first_run == 1
+        flag_first_run = 0;
+        imwrite(A, map, filename,'gif', 'LoopCount', Inf, 'DelayTime', 1);
+    else
+        imwrite(A, map, filename, 'gif', 'WriteMode', 'append', ...
+                'DelayTime', 1);
+    end
+end
 
 % imshow(im)
-hold on
-axis equal
-hold off
-imshow(im_out)
+% hold on
+% axis equal
+% hold off
+% imshow(im_out)
