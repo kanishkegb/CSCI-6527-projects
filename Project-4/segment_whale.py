576ae692a5738b2b067c6a4e27c82268a643f3a3
Kanishke-Mac <kanishkegb@gwu.edu>
2018-04-27 17:23:24 -0400

    resize to smaller grayscale images

--- Project-4/segment_whale.py
+++ Project-4/segment_whale.py
@@ -3,36 +3,45 @@ import cv2
 import pdb
 from matplotlib import pyplot as plt
 
-img = cv2.imread('../../Whale_ID/train/0a5c0f48.jpg')
-# pdb.set_trace()
+img = cv2.imread('../../Whale_ID/train/0a3b0493.jpg')
 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+img[:, :, 1] = 0
+img[:, :, 0] = 0
 
-# noise removal
-kernel = np.ones((3,3),np.uint8)
-opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# sure background area
-sure_bg = cv2.dilate(opening,kernel,iterations=3)
-
-# Finding sure foreground area
-dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
-
-# Finding unknown region
-sure_fg = np.uint8(sure_fg)
-unknown = cv2.subtract(sure_bg,sure_fg)
-
-# Marker labelling
-ret, markers = cv2.connectedComponents(sure_fg)
-# Add one to all labels so that sure background is not 0, but 1
-markers = markers+1
-# Now, mark the region of unknown with zero
-markers[unknown==255] = 0
-
-markers = cv2.watershed(img,markers)
-img[markers == -1] = [255,0,0]
-
-plt.imshow(img)
+plt.imshow(cv2.cvtColor(hsv, cv2.COLOR_BGR2RGB))
 plt.show()
+#
+# gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# # pdb.set_trace()
+# # gray = img
+# ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+#
+# # noise removal
+# kernel = np.ones((3,3),np.uint8)
+# opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+#
+# # sure background area
+# sure_bg = cv2.dilate(opening,kernel,iterations=3)
+#
+# # Finding sure foreground area
+# dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+# ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+#
+# # Finding unknown region
+# sure_fg = np.uint8(sure_fg)
+# unknown = cv2.subtract(sure_bg,sure_fg)
+#
+# # Marker labelling
+# ret, markers = cv2.connectedComponents(sure_fg)
+# # Add one to all labels so that sure background is not 0, but 1
+# markers = markers+1
+# # Now, mark the region of unknown with zero
+# markers[unknown==255] = 0
+#
+# markers = cv2.watershed(img,markers)
+# img[markers == -1] = [255,0,0]
+#
+# plt.imshow(img)
+# plt.show()
