2bf297d0b28cd35995b2654c7cf534161f9394d5
Kani-Desktop <kanishkegb@gwu.edu>
2018-05-06 20:13:59 -0400

    identify cropped whales reads train data

--- Project-4/data_io.py
+++ Project-4/data_io.py
@@ -148,6 +148,164 @@ def load_test_data(path_prefix, read_data_again, read_all_data):
 
     return whale_image_names, whale_images
 
+def load_cropped_train_data(path_prefix, read_data_again, read_all_data, skip_new_whales):
+    '''
+    Loads training data from the files provided from Kaggle
+
+    Args:
+        path_prefix: string - path to the data from Kaggle
+        read_data_again: bool - whether to read data from the file again
+        read_all_data: bool -  whether to read data all data
+        skip_new_whales: bool - whether to skip the 'new_whale' IDs
+    Returns:
+        whale_id: list - IDs for each whale image
+        whale_image_names: list - name of the image
+        whale_images: list - list of NxMx3 arrays for each whale image
+    '''
+
+    print('Loading data ...')
+    data_file = os.path.join('..', '..', 'Whale_ID', 'train.csv')
+    data = pd.read_csv(data_file)
+    whale_image_names = data['Image']
+    whale_id = data['Id']
+    n_samples = len(whale_id)
+
+    if read_data_again:
+        print('Checking cropped data ...')
+        cropped_images = []
+        for im in os.listdir('cropped_train_images'):
+            if im.endswith('.jpg'):
+                cropped_images.append(im)
+        print('Cropped images: {}'.format(len(cropped_images)))
+
+        print('Saving {} items into hdf5 ...'.format(n_samples))
+        with h5py.File('cropped_train_data.hdf5', 'w') as f:
+            grp_id = f.create_group('whale_id')
+            grp_im_name = f.create_group('image_name')
+            grp_im = f.create_group('whale_images')
+            skip_count = 0
+            for i in range(n_samples):
+                image_name = whale_image_names[i]
+                image_path = os.path.join('..', '..', 'Whale_ID', 'train', image_name)
+                image = cv2.imread(image_path)
+                id = whale_id[i]
+                # print('i={},\tID={}\n'.format(i, id))
+
+                if not image_name in cropped_images:
+                    print('skipping {}'.format(image_name))
+                    skip_count += 1
+                    id = 'new_whale'
+
+                grp_id.create_dataset('{}'.format(i), data=id)
+                grp_im_name.create_dataset('{}'.format(i), data=image_name)
+                grp_im.create_dataset('{}'.format(i), data=image)
+                # whale_images.append(image.reshape(1, -1)[0])
+            print('No whales detected in {} images'.format(skip_count))
+        print('Saving data into hdf5 completed.')
+
+    whale_images = []
+    whale_image_names = []
+    whale_id = []
+    max_data = 100
+
+    if read_all_data:
+        print('Reading all data from hdf5 file ...')
+    else:
+        print('Reading first {} data from hdf5 file ...'.format(max_data))
+
+    with h5py.File('cropped_train_data.hdf5', 'r') as f:
+        i = 0
+        for key in f['whale_images'].keys():
+            if skip_new_whales and f['whale_id'][key].value=='new_whale':
+                continue
+            whale_images.append(f['whale_images'][key].value)
+            i += 1
+            if not read_all_data and i > max_data:
+                break
+
+        i = 0
+        for key in f['image_name'].keys():
+            if skip_new_whales and f['whale_id'][key].value=='new_whale':
+                continue
+            whale_image_names.append(f['image_name'][key].value)
+            i += 1
+            if not read_all_data and i > max_data:
+                break
+
+        i = 0
+        for key in f['whale_id'].keys():
+            if skip_new_whales and f['whale_id'][key].value=='new_whale':
+                continue
+            whale_id.append(f['whale_id'][key].value)
+            i += 1
+            if not read_all_data and i > max_data:
+                break
+
+    print('Reading data completed.')
+
+    return whale_id, whale_image_names, whale_images
+
+
+def load_cropped_test_data(path_prefix, read_data_again, read_all_data):
+    '''
+    Loads test data from the files provided from Kaggle
+
+    Args:
+        path_prefix: string - path to the data from Kaggle
+        read_data_again: bool - whether to read data from the file again
+        read_all_data: bool -  whether to read data all data
+    Returns:
+        whale_image_names: list - name of the image
+        whale_images: list - list of NxMx3 arrays for each whale image
+    '''
+
+    if read_data_again:
+        print('Saving data into hdf5 ...')
+        with h5py.File('whale_test_data.hdf5', 'w') as f:
+            grp_im_name = f.create_group('image_name')
+            grp_im = f.create_group('test_images')
+            i = 0
+            for im in os.listdir('{}test/'.format(path_prefix)):
+                if im.endswith('.jpg'):
+                    image = cv2.imread('{}test/{}'.format(path_prefix, im))
+                    grp_im.create_dataset('{}'.format(i), data=image)
+                    grp_im_name.create_dataset('{}'.format(i), data=im)
+                    i += 1
+
+        print('Saving {} data into hdf5 completed.'.format(i))
+
+
+    print('Loading test data ...')
+    whale_images = []
+    whale_image_names = []
+
+    max_data = 100
+
+    if read_all_data:
+        print('Reading all test data from hdf5 file ...')
+    else:
+        print('Reading first {} test data from hdf5 file ...'.format(max_data))
+
+    with h5py.File('whale_test_data.hdf5', 'r') as f:
+        i = 0
+        for key in f['test_images'].keys():
+            whale_images.append(f['test_images'][key].value)
+            i += 1
+            if not read_all_data and i > max_data:
+                break
+
+        i = 0
+        for key in f['image_name'].keys():
+            whale_image_names.append(f['image_name'][key].value)
+            i += 1
+            if not read_all_data and i > max_data:
+                break
+
+
+    print('Reading data completed.')
+
+    return whale_image_names, whale_images
+
 
 if __name__ == '__main__':
     # path_prefix = '../../Whale_ID/'
@@ -155,11 +313,18 @@ if __name__ == '__main__':
     # read_all_data = False
     # id, image_names, images = load_data(path_prefix, read_data_again,
     #                                     read_all_data)
-    path_prefix = '../../Whale_ID/'
-    read_data_again = False
-    read_all_data = False
-    image_names, images = load_test_data(path_prefix, read_data_again,
-                                         read_all_data)
+    # path_prefix = '../../Whale_ID/'
+    # read_data_again = False
+    # read_all_data = False
+    # image_names, images = load_test_data(path_prefix, read_data_again,
+    #                                      read_all_data)
+    #
+    path_prefix = ''
+    read_data_again = True
+    read_all_data = True
+    skip_new_whales = False
+    whale_id, whale_image_names, whale_images = load_cropped_train_data(path_prefix,
+        read_data_again, read_all_data, skip_new_whales)
 
     pdb.set_trace()
     # plt.imshow(cv2.cvtColor(images[0], cv2.COLOR_BGR2RGB))
---
+++ Project-4/identify_cropped_whales.py
@@ -0,0 +1,135 @@
+from sklearn import svm, metrics
+from data_io import load_data, load_test_data
+
+import argparse
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import pdb
+
+
+# plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+# plt.title('Whale: {}, ID: {}'.format(image_name, id))
+# plt.show()
+
+def train_classifier(ids, image_names, images):
+    n_samples = len(ids)
+
+    print('Running the classifier...')
+    resize_h = 100
+    resize_w = 120
+
+    whales = np.zeros((n_samples, resize_h * resize_w))
+    i = 0
+    for image in images:
+        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        resized_image = cv2.resize(gray, (resize_h, resize_w))
+        whales[i, :] = resized_image.reshape(1, -1)[0].astype(np.float64)
+        i += 1
+
+    classifier = svm.SVC(verbose=True)#, probability=True)
+    classifier.fit(whales, ids)
+
+    print('Predicting ...')
+    expected = ids[n_samples // 200:]
+    predicted = classifier.predict(whales[n_samples // 200:])
+
+    print('Classification report for classifier {}:\n{}\n'.format(
+          classifier,  metrics.classification_report(expected, predicted)
+          ))
+    print('Confusion matrix:\n{}'.format(metrics.confusion_matrix(expected,
+          predicted)))
+
+    return classifier, whales
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description=(
+            'Categorizes whales from their flukes using machine'
+            'learning'))
+    parser.add_argument(
+        '-p', '--path',
+        default='../../Whale_ID/',
+        help='specify the path to the data downloaded from Kaggle'
+    )
+    parser.add_argument(
+        '-r', '--read_data_again',
+        default=False, action='store_true',
+        help=('Read data again from the downloaded data. By default '
+              'this reads from the saved hdf5 file')
+    )
+    parser.add_argument(
+        '-a', '--read_all_data',
+        default=False, action='store_true',
+        help=('Read all data from the downloaded data. By default '
+              'this reads only first 100')
+    )
+    parser.add_argument(
+        '-t', '--train_data',
+        default=False, action='store_true',
+        help=('Train the classifier')
+    )
+
+    args = parser.parse_args()
+    path_prefix = args.path
+    read_data_again = args.read_data_again
+    read_all_data = args.read_all_data
+    train_data = args.train_data
+
+    skip_new_whales = True
+
+    ids, image_names, images = load_data(path_prefix, read_data_again,
+    read_all_data, skip_new_whales)
+
+    pdb.set_trace()
+    if train_data:
+        clf, whales = train_classifier(ids, image_names, images)
+        with open('trained_classifier.pkl', 'wb') as f:
+            pickle.dump(clf, f)
+    else:
+        with open('trained_classifier.pkl', 'rb') as f:
+            clf = pickle.load(f)
+
+    test_image_names, test_images = load_test_data(path_prefix, read_data_again,
+                                                   read_all_data)
+    print('Predicting ...')
+    n_samples= len(test_images)
+    resize_h = 100
+    resize_w = 120
+    test_whales = np.zeros((n_samples, resize_h * resize_w))
+    i = 0
+    for image in test_images:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        resized_image = cv2.resize(gray, (resize_h, resize_w))
+        test_whales[i, :] = resized_image.reshape(1, -1)[0].astype(np.float64)
+        i += 1
+
+    i = 0
+    train_whales = np.zeros((len(ids), resize_h * resize_w))
+    for image in images:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        resized_image = cv2.resize(gray, (resize_h, resize_w))
+        train_whales[i, :] = resized_image.reshape(1, -1)[0].astype(np.float64)
+        i += 1
+
+    expected = ids[0:1]
+    predicted = clf.predict(train_whales[:1])
+
+    print('Classification report for classifier {}:\n{}\n'.format(
+          clf,  metrics.classification_report(expected, predicted)
+          ))
+    print('Confusion matrix:\n{}'.format(metrics.confusion_matrix(expected,
+          predicted)))
+    
+    predicted = clf.predict(test_whales) 
+    with open('predicted_test_whales.pkl', 'wb') as f:
+        pickle.dump(predicted, f)
+        pickle.dump(test_image_names, f)
+
+    out_data = {'id':predicted, 'image':test_image_names}
+    df = pd.DataFrame(data=out_data)
+    df.to_csv('predictions.csv', index=False)
+    pdb.set_trace()
